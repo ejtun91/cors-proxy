44d34fdbcd525029e7d9b652f96c5ac2bbe45e02
Antonio K (ejtun91) <ejtun91@gmail.com>
2024-01-13 18:16:24 +0100

    add cron for render

--- server.js
+++ server.js
@@ -3,21 +3,27 @@ var port = process.env.PORT || 8080;
 
 var cron = require("node-cron");
 var cors_proxy = require("cors-anywhere");
+var server;
 
 function startServer() {
-  cors_proxy
-    .createServer({
-      originWhitelist: [], // Allow all origins
-    })
-    .listen(port, host, function () {
-      console.log("Server is running on " + host + ":" + port);
-    });
+  server = cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+  });
+  server.listen(port, host, function () {
+    console.log("Server is running on " + host + ":" + port);
+  });
 }
 
 startServer();
 
 cron.schedule("*/13 * * * *", function () {
-  console.log("Cron job executed at", new Date());
+  if (server) {
+    server.close(function () {
+      startServer();
+    });
+  } else {
+    startServer();
+  }
 
   startServer();
 });
